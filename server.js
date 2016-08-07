7bf71d14f05767eb5b9af8e5cd62f837ceee5220
Max <max_zavernenkov@mail.ru>
2016-08-07 14:56:28 +0300

    Almost finished Flux part of tutorial

--- server.js
+++ server.js
@@ -1,7 +1,24 @@
 import express from 'express';
 
+import {MongoClient} from 'mongodb';
+let MONGO_URL = 'mongodb://zavernutiy:max5187652@ds145355.mlab.com:45355/rgrjs_pluralsight';
+
 let app = express();
 
 app.use(express.static('public'));
 
-app.listen(3000);
+let db;
+MongoClient.connect(process.env.MONGO_URL || MONGO_URL, (err, database) => {
+    if (err) throw err;
+
+    db = database;
+    app.listen(3000, () => console.log('listening on port 3000'));
+});
+
+app.get('/data/links', (req, res) => {
+    db.collection('links').find({}).toArray((err, links) => {
+        if (err) throw err;
+
+        res.json(links);
+    });
+});
