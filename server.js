409f286f6b70d8121be95e9e6f7bc46fa75c3c15
Max Zavernenkov <zavernutiy@Mac-mini-de-TMW.local>
2016-08-08 15:27:01 +0300

    GraphQL easy example

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import schema from './data/schema';
+import GraphQLHTTP from 'express-graphql';
 
 import {MongoClient} from 'mongodb';
 let MONGO_URL = 'mongodb://zavernutiy:max5187652@ds145355.mlab.com:45355/rgrjs_pluralsight';
@@ -6,6 +8,11 @@ let MONGO_URL = 'mongodb://zavernutiy:max5187652@ds145355.mlab.com:45355/rgrjs_p
 let app = express();
 
 app.use(express.static('public'));
+
+app.use('/graphql', GraphQLHTTP({
+    schema,
+    graphiql: true
+}));
 
 let db;
 MongoClient.connect(process.env.MONGO_URL || MONGO_URL, (err, database) => {
