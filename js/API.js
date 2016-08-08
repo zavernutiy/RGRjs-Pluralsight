685f091f26a5651ab2d67568029d52cc2c70f3d9
Max Zavernenkov <zavernutiy@Mac-mini-de-TMW.local>
2016-08-08 16:18:30 +0300

    Finished graphicql part

--- js/API.js
+++ js/API.js
@@ -1,12 +1,20 @@
-import {get} from 'jquery';
+import {post} from 'jquery';
 import ServerActions from './actions/ServerActions';
 
 let API = {
     fetchLinks() {
         console.log('1. In API');
         // Ajax request to /data/links
-        get('data/links').done(resp => {
-            ServerActions.receiveLinks(resp);
+        post('/graphql', {
+            query: `{
+                links {
+                    _id,
+                    title,
+                    url
+                }
+            }`
+        }).done(resp => {
+            ServerActions.receiveLinks(resp.data.links);
         });
     }
 };
--- server.js
+++ server.js
@@ -9,23 +9,23 @@ let app = express();
 
 app.use(express.static('public'));
 
-app.use('/graphql', GraphQLHTTP({
-    schema,
-    graphiql: true
-}));
-
 let db;
 MongoClient.connect(process.env.MONGO_URL || MONGO_URL, (err, database) => {
     if (err) throw err;
 
     db = database;
+    app.use('/graphql', GraphQLHTTP({
+        schema: schema(db),
+        graphiql: true
+    }));
     app.listen(3000, () => console.log('listening on port 3000'));
 });
 
+/*
 app.get('/data/links', (req, res) => {
     db.collection('links').find({}).toArray((err, links) => {
         if (err) throw err;
 
         res.json(links);
     });
-});
+});*/
